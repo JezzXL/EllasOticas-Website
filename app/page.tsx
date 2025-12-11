d32326af479eec91f6bc325762c56c197a002ef6
unknown <kionegamesyotuber337@gmail.com>
2025-12-11 20:57:46 -0300

    feat: add Categories to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Hero from "@/components/home/Hero";
 import FeaturedProducts from "@/components/home/FeaturedProducts";
+import Categories from "@/components/home/Categories";
+import Brands from "@/components/home/Brands";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <FeaturedProducts />
+      <Categories />
+      <Brands />
       {/* Outros componentes serão adicionados aqui */}
     </main>
   );
